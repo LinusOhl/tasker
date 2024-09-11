40485977794955ee4bec9d84eb6c97ea6da21199
Linuss <tade3d@gmail.com>
2024-09-11 09:51:45 +0200

    Basic helper functions for tasks.

--- client/src/helpers.ts
+++ client/src/helpers.ts
@@ -1,1 +1,33 @@
 export const BASE_URL = "http://localhost:3000";
+
+export enum TASK_STATUS {
+  NOT_STARTED = "NOT_STARTED",
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED",
+}
+
+export const getTaskStatusEnum = (status: string) => {
+  switch (status) {
+    case "Not started":
+      return TASK_STATUS.NOT_STARTED;
+    case "In progress":
+      return TASK_STATUS.IN_PROGRESS;
+    case "Completed":
+      return TASK_STATUS.COMPLETED;
+    default:
+      return TASK_STATUS.NOT_STARTED;
+  }
+};
+
+export const getTaskPriorityNbr = (priority: string) => {
+  switch (priority) {
+    case "Low":
+      return 0;
+    case "Medium":
+      return 1;
+    case "High":
+      return 2;
+    default:
+      return 0;
+  }
+};

3b9501f2d68c38629b7b1f91e876fa92ff9a7f6d
Linuss <tade3d@gmail.com>
2024-09-11 09:52:37 +0200

    Updated home.tsx.

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,17 +1,38 @@
-import { Title } from "@mantine/core";
-import { createFileRoute } from "@tanstack/react-router";
+import { Button, Title } from "@mantine/core";
+import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
+import { TaskCreationModal } from "../components/organisms/TaskCreationModal/TaskCreationModal";
 import { TasksTabsNavigation } from "../features/Tasks/components/TasksTabsNavigation";
+import { useAuth } from "../hooks/useAuth";
 
 const Home = () => {
+  const { user: userFromHook, signOut } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    await signOut();
+    navigate({ to: "/" });
+  };
+
   return (
     <div>
-      <Title order={1}>Home</Title>
+      <Title order={1}>Hiya, {userFromHook?.email}</Title>
+
+      <Button onClick={handleSignOut}>Sign out</Button>
 
       <TasksTabsNavigation />
+
+      <TaskCreationModal />
     </div>
   );
 };
 
 export const Route = createFileRoute("/home")({
+  beforeLoad: ({ context }) => {
+    if (!context.auth.user) {
+      throw redirect({
+        to: "/",
+      });
+    }
+  },
   component: Home,
 });

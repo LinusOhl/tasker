515ec6d16107ff71aca2314e7bfc8f7a71997c47
Linuss <tade3d@gmail.com>
2024-09-17 16:15:52 +0200

    Created page view for a task.
    Created the page for viewing the details of a single task.

---
+++ client/src/routes/tasks.$taskId.tsx
@@ -0,0 +1,80 @@
+import { Avatar, Box, Button, Center, Flex, Text, Title } from "@mantine/core";
+import { createFileRoute } from "@tanstack/react-router";
+import { Priority } from "../components/atom/Priority/Priority";
+import { Status } from "../components/atom/Status/Status";
+import { TASK_STATUS } from "../helpers";
+import { useTaskById } from "../hooks/useTaskById";
+import { useUpdateTask } from "../hooks/useUpdateTask";
+
+const TaskView = () => {
+  const { taskId } = Route.useParams();
+
+  const { data } = useTaskById(taskId);
+
+  const updateTaskMutation = useUpdateTask();
+
+  const handleCompleteTask = () => {
+    updateTaskMutation.mutate({
+      taskId: taskId,
+      taskData: { status: TASK_STATUS.COMPLETED },
+    });
+  };
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Box px={"md"}>
+      {/* Status and priority */}
+      <Center py={"xl"}>
+        <Flex direction={"column"} align={"center"} gap={"xs"}>
+          <Flex align={"center"} gap={"xl"}>
+            <Status status={data.status} />
+            <Priority priority={String(data.priority)} />
+          </Flex>
+
+          <Text size="sm" c={"dimmed"}>
+            {data.createdAt}
+          </Text>
+        </Flex>
+      </Center>
+
+      {/* Title and description */}
+      <Box>
+        <Title>{data.title}</Title>
+        {data.description && <Text size="lg">{data.description}</Text>}
+      </Box>
+
+      {/* Avatar */}
+      <Flex mt={"lg"} gap={"xs"} align={"center"}>
+        <Avatar color="cyan" radius={"sm"} size={"lg"}>
+          {data.createdById.slice(0, 2)}
+        </Avatar>
+
+        <Flex direction={"column"}>
+          <Text size="md" fw={500} c={"dimmed"}>
+            Assigned to
+          </Text>
+          <Text size="xl" fw={500}>
+            {data.createdById.slice(0, 8)}
+          </Text>
+        </Flex>
+      </Flex>
+
+      {/* Actions */}
+      <Flex direction={"column"} mt={"xl"} gap={"sm"}>
+        <Button size="lg" variant="outline" fullWidth>
+          Edit task
+        </Button>
+        <Button size="lg" onClick={handleCompleteTask} fullWidth>
+          Complete task
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
+
+export const Route = createFileRoute("/tasks/$taskId")({
+  component: TaskView,
+});

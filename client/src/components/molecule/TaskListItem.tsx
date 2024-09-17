e6442932fc593512c14088e471fbf042833b80b3
Linuss <tade3d@gmail.com>
2024-09-17 16:10:20 +0200

    Moved files around.

--- client/src/components/molecule/TaskListItem.tsx
+++ client/src/components/molecule/TaskListItem.tsx
@@ -1,6 +1,5 @@
 import {
   Avatar,
-  Badge,
   Box,
   Flex,
   Text,
@@ -8,11 +7,13 @@ import {
   Title,
   Tooltip,
 } from "@mantine/core";
+import { useNavigate } from "@tanstack/react-router";
 import { FaChevronRight } from "react-icons/fa6";
-import { Priority } from "../../atom/Priority/Priority";
-import { getStatusColor, getStatusText } from "./TaskListItem.helper";
+import { Priority } from "../atom/Priority/Priority";
+import { Status } from "../atom/Status/Status";
 
 interface TaskListItemProps {
+  id: string;
   title: string;
   description: string;
   status: string;
@@ -21,12 +22,15 @@ interface TaskListItemProps {
 }
 
 export const TaskListItem = ({
+  id,
   title,
   description,
   status,
   assignee,
   priority,
 }: TaskListItemProps) => {
+  const navigate = useNavigate();
+
   return (
     <Flex direction={"column"} p={"xs"}>
       {/* Header */}
@@ -46,16 +50,18 @@ export const TaskListItem = ({
           <Text>{description}</Text>
         </Flex>
 
-        <ThemeIcon variant="transparent" c={"black"}>
+        <ThemeIcon
+          variant="transparent"
+          c={"black"}
+          onClick={() => navigate({ to: `/tasks/${id}` })}
+        >
           <FaChevronRight />
         </ThemeIcon>
       </Flex>
 
       {/* Footer */}
       <Flex gap={"xs"} justify={"space-between"} align={"center"}>
-        <Badge size="md" variant="light" color={getStatusColor(status)}>
-          {getStatusText(status)}
-        </Badge>
+        <Status status={status} />
 
         {Array.isArray(assignee) ? (
           <Tooltip.Group>

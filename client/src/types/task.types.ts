c1821544ec742e4615c82c750e933914bb1f5d2e
Linuss <tade3d@gmail.com>
2024-09-18 11:42:11 +0200

    Changed to make use of enums.

--- client/src/types/task.types.ts
+++ client/src/types/task.types.ts
@@ -1,3 +1,5 @@
+import type { TASK_PRIORITY, TASK_STATUS } from "../helpers";
+
 export type Task = {
   id: string;
   createdAt: string;
@@ -12,8 +14,8 @@ export type Task = {
 export type UpdateTaskData = {
   title?: string;
   description?: string;
-  status?: string;
-  priority?: number;
+  status?: TASK_STATUS;
+  priority?: TASK_PRIORITY;
 };
 
 export type CreateTaskData = Omit<Task, "id" | "createdAt" | "updatedAt">;

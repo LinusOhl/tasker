47218688769873730f3746a1236f2679989bcb7b
Linuss <tade3d@gmail.com>
2024-09-23 13:21:54 +0200

    Implemented task search.
    Possible to search for tasks by task titles,, returns a filtered array of tasks or "No tasks found".

--- client/src/components/organisms/OpenTasksPanel/OpenTasksPanel.tsx
+++ client/src/components/organisms/OpenTasksPanel/OpenTasksPanel.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Select, TextInput, Title } from "@mantine/core";
+import { Box, Flex, Select, Text, TextInput, Title } from "@mantine/core";
+import { useMemo, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useAuth } from "../../../hooks/useAuth";
 import { useTasksByUserId } from "../../../hooks/useTasksByUserId";
@@ -8,14 +9,31 @@ import classes from "./OpenTasksPanel.module.scss";
 export const OpenTasksPanel = () => {
   const auth = useAuth();
 
-  const { data } = useTasksByUserId(auth.user?.id ?? "");
+  const { data: tasks } = useTasksByUserId(auth.user?.id ?? "");
+
+  // State for search filtering
+  const [searchInput, setSearchInput] = useState("");
+
+  // Returns all tasks if no searchInput, otherwise the filtered tasks.
+  const filteredTasks = useMemo(() => {
+    if (!searchInput) return tasks;
+
+    return tasks?.filter((task) =>
+      task.title.toLowerCase().includes(searchInput.toLowerCase()),
+    );
+  }, [searchInput, tasks]);
 
   return (
     <div>
       <Box px={"xs"} mt={"md"}>
         <Title order={2}>Open tasks</Title>
 
-        <TextInput placeholder="Search" leftSection={<FaSearch />} />
+        <TextInput
+          placeholder="Search"
+          leftSection={<FaSearch />}
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
 
         <Flex gap={"xs"}>
           <Select
@@ -34,17 +52,21 @@ export const OpenTasksPanel = () => {
       </Box>
 
       <Flex direction={"column"} className={classes.tasksContainer}>
-        {data?.map((task) => (
-          <TaskListItem
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            description={task.description ?? ""}
-            status={task.status}
-            assignee={task.createdById.slice(0, 2)}
-            priority={String(task.priority)}
-          />
-        ))}
+        {filteredTasks?.length ? (
+          filteredTasks.map((task) => (
+            <TaskListItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              description={task.description ?? ""}
+              status={task.status}
+              assignee={task.createdById.slice(0, 2)}
+              priority={String(task.priority)}
+            />
+          ))
+        ) : (
+          <Text>No tasks found.</Text>
+        )}
       </Flex>
     </div>
   );
